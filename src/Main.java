61bfd44b2081ef2577a76598a942b6830d707dfc
Abdulkadir Karslı <84068137+akarsli@users.noreply.github.com>
2024-05-23 22:01:15 +0300

    menubar eklendi.java

--- src/Main.java
+++ src/Main.java
@@ -24,6 +24,10 @@ public class Main {
         addLabelBtn.setBounds(15, 90, 150, 30);
         pane.add(addLabelBtn);
 
+        JButton addMenuBar=new JButton("Menü Bar Ekle");
+        addMenuBar.setBounds(15,130,150,30);
+        pane.add(addMenuBar);
+
         JPanel dropArea = new JPanel();
         dropArea.setBounds(250, 50, frame.getWidth() - 320, frame.getHeight() - 150);
         dropArea.setBorder(BorderFactory.createLineBorder(Color.BLACK, 3));
@@ -37,23 +41,23 @@ public class Main {
         separator.setOpaque(true);
         pane.add(separator);
 
+        add add = new add(dropArea);
+
         frame.addComponentListener(new ComponentAdapter() {
             @Override
             public void componentResized(ComponentEvent e) {
                 int newHeight = frame.getHeight();
                 int newWidth = frame.getWidth();
 
-                separator.setBounds(200, 0, 1, newHeight); // separator yüksekliğini güncelle
-                dropArea.setBounds(250, 50, newWidth - 320, newHeight - 150); // dropArea genişliğini güncelle
+                separator.setBounds(200, 0, 1, newHeight);
+                dropArea.setBounds(250, 50, newWidth - 320, newHeight - 150);
+                add.adjustMenuBarSize(dropArea.getWidth());
             }
         });
 
-
-
-Timer timer = new Timer(100, e -> separator.setBounds(200, 0, 1, frame.getHeight()));
+        Timer timer = new Timer(100, e -> separator.setBounds(200, 0, 1, frame.getHeight()));
         timer.start();
 
-        add add = new add(dropArea);
 
         btn.addMouseListener(new MouseAdapter() {
             @Override
@@ -76,6 +80,18 @@ Timer timer = new Timer(100, e -> separator.setBounds(200, 0, 1, frame.getHeight
             }
         });
 
+        addMenuBar.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                if (add.isBlnMenu()==false) {
+                    add.createMenuBar();
+                } else {
+                    JPopupMenu popupMenu = new JPopupMenu();
+                    JOptionPane.showMessageDialog(pane,"MenuBar daha önce oluşturulmuş");
+                    popupMenu.show(addMenuBar, e.getX(), e.getY());
+                }
+            }
+        });
 
         frame.setFocusable(true);
         frame.setVisible(true);
