3ad69c74a788531fa40a6c34403c7c6821cf9fe1
sevban <sevbanbozaslan30@gmail.com>
2024-05-25 10:03:37 +0300

    gereksiz boşluklar silindi

--- src/CodeGenerator.java
+++ src/CodeGenerator.java
@@ -20,63 +20,59 @@ public class CodeGenerator implements ActionListener {
                 import java.awt.*;
                 import java.awt.event.*;
                 public class Main {
-                    public static void main(String[] args) {
-                        JFrame frame = new JFrame("GUI");
-                        Container pane = frame.getContentPane();
-                        frame.setLayout(null);
-                        frame.setSize(%d ,%d);
+                public static void main(String[] args) {
+                JFrame frame = new JFrame("GUI");
+                Container pane = frame.getContentPane();
+                frame.setLayout(null);
+                frame.setSize(%d ,%d);
                 """, width, height));
 
         for (Component comp : dropArea.getComponents()) {
             if (comp instanceof JButton) {
                 JButton button = (JButton) comp;
-                code.append(generateButtonCode(button)).append("\n");
+                code.append(generateButtonCode(button));
             } else if (comp instanceof JTextField) {
                 JTextField textField = (JTextField) comp;
-                code.append(generateTextFieldCode(textField)).append("\n");
+                code.append(generateTextFieldCode(textField));
             } else if (comp instanceof JLabel) {
                 JLabel label = (JLabel) comp;
-                code.append(generateLabelCode(label)).append("\n");
+                code.append(generateLabelCode(label));
             } else if (comp instanceof JMenuBar) {
                 JMenuBar menubar = (JMenuBar) comp;
-                code.append(generateMenuBarCode(menubar)).append("\n");
+                code.append(generateMenuBarCode(menubar));
             } else if (comp instanceof JComboBox) {
                 JComboBox<?> comboBox = (JComboBox<?>) comp;
-                code.append(generateComboBoxCode(comboBox)).append("\n");
+                code.append(generateComboBoxCode(comboBox));
             }
         }
         code.append("""
                 frame.addComponentListener(new ComponentAdapter() {
-                    @Override
-                    public void componentResized(ComponentEvent e) {
-                        int newHeight = frame.getHeight();
-                        int newWidth = frame.getWidth();
-                        menubar.setBounds(0, 0, frame.getWidth(), 30);
-                    }
-                });
+                @Override
+                public void componentResized(ComponentEvent e) {
+                int newHeight = frame.getHeight();
+                int newWidth = frame.getWidth();
+                menubar.setBounds(0, 0, frame.getWidth(), 30);
+                }});
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 frame.setFocusable(true);
                 frame.setVisible(true);
                 }}
                 class CustomMenuBar extends JMenuBar {
-                    @Override
-                    protected void paintComponent(Graphics g) {
-                        super.paintComponent(g);
-                        Graphics2D g2d = (Graphics2D) g;
-                        g2d.setColor(Color.BLACK);
-                        for (int i = 0; i < getMenuCount(); i++) {
-                            JMenu menu = getMenu(i);
-                            if (menu != null) {
-                                Rectangle bounds = menu.getBounds();
-                                g2d.drawRect(bounds.x, bounds.y, bounds.width, bounds.height);
-                                // Remove left border of the first menu
-                                if (i == 0) {
-                                    g2d.clearRect(bounds.x, bounds.y, 1, bounds.height);
-                                }
-                            }
-                        }
-                    }
-                }""");
+                @Override
+                protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Graphics2D g2d = (Graphics2D) g;
+                g2d.setColor(Color.BLACK);
+                for (int i = 0; i < getMenuCount(); i++) {
+                JMenu menu = getMenu(i);
+                if (menu != null) {
+                Rectangle bounds = menu.getBounds();
+                g2d.drawRect(bounds.x, bounds.y, bounds.width, bounds.height);
+                // Remove left border of the first menu
+                if (i == 0) {
+                g2d.clearRect(bounds.x, bounds.y, 1, bounds.height);
+                }}}}}
+                """);
 
         JTextArea textArea = new JTextArea(code.toString());
         textArea.setLineWrap(true);
@@ -92,7 +88,7 @@ public class CodeGenerator implements ActionListener {
         return String.format(
                 "JButton button%d = new JButton(\"%s\");\n" +
                         "button%d.setBounds(%d, %d, %d, %d);\n" +
-                        "pane.add(button%d);",
+                        "pane.add(button%d);\n",
                 button.hashCode(), button.getText(),
                 button.hashCode(), button.getX(), button.getY(), button.getWidth(), button.getHeight(),
                 button.hashCode()
@@ -103,7 +99,7 @@ public class CodeGenerator implements ActionListener {
         return String.format(
                 "JTextField textField%d = new JTextField(\"%s\");\n" +
                         "textField%d.setBounds(%d, %d, %d, %d);\n" +
-                        "pane.add(textField%d);",
+                        "pane.add(textField%d);\n",
                 textField.hashCode(), textField.getText(),
                 textField.hashCode(), textField.getX(), textField.getY(), textField.getWidth(), textField.getHeight(),
                 textField.hashCode()
@@ -114,7 +110,7 @@ public class CodeGenerator implements ActionListener {
         return String.format(
                 "JLabel label%d = new JLabel(\"%s\");\n" +
                         "label%d.setBounds(%d, %d, %d, %d);\n" +
-                        "pane.add(label%d);",
+                        "pane.add(label%d);\n",
                 label.hashCode(), label.getText(),
                 label.hashCode(), label.getX(), label.getY(), label.getWidth(), label.getHeight(),
                 label.hashCode()
@@ -131,7 +127,7 @@ public class CodeGenerator implements ActionListener {
 
         for (int i = 0; i < menubar.getMenuCount(); i++) {
             JMenu menu = menubar.getMenu(i);
-            menuBarCode.append(generateMenuCode(menu)).append("\n");
+            menuBarCode.append(generateMenuCode(menu));
         }
 
         menuBarCode.append("pane.add(menubar);");
@@ -150,7 +146,7 @@ public class CodeGenerator implements ActionListener {
         for (int i = 0; i < menu.getItemCount(); i++) {
             JMenuItem menuItem = menu.getItem(i);
             if (menuItem != null) {
-                menuCode.append(generateMenuItemCode(menuItem, menu.hashCode())).append("\n");
+                menuCode.append(generateMenuItemCode(menuItem, menu.hashCode()));
             }
         }
 
@@ -160,7 +156,7 @@ public class CodeGenerator implements ActionListener {
     private String generateMenuItemCode(JMenuItem menuItem, int menuHashCode) {
         return String.format(
                 "JMenuItem menuItem%d = new JMenuItem(\"%s\");\n" +
-                        "menu%d.add(menuItem%d);",
+                        "menu%d.add(menuItem%d);\n",
                 menuItem.hashCode(), menuItem.getText(),
                 menuHashCode, menuItem.hashCode()
         );
@@ -180,7 +176,7 @@ public class CodeGenerator implements ActionListener {
         }
         comboBoxCode.append(String.format(
                 "});\ncomboBox%d.setBounds(%d, %d, %d, %d);\n" +
-                        "pane.add(comboBox%d);",
+                        "pane.add(comboBox%d);\n",
                 comboBox.hashCode(), comboBox.getX(), comboBox.getY(), comboBox.getWidth(), comboBox.getHeight(),
                 comboBox.hashCode()
         ));
