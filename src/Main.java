0ae1d210248e14ce4b6f1937eb7345050a245c3f
Selim Genç <selimgnc44@gmail.com>
2024-05-25 16:09:31 +0300

    MAIN DUZENLENDI

--- src/Main.java
+++ src/Main.java
@@ -4,76 +4,6 @@ import java.awt.event.*;
 
 public class Main {
 
-    public static class GridPanel extends JPanel {
-        private int gridSize;
-        private boolean showGrid;
-
-        public GridPanel(int gridSize) {
-            this.gridSize = gridSize;
-            this.showGrid = true;
-
-            addMouseListener(new MouseAdapter() {
-                @Override
-                public void mousePressed(MouseEvent e) {
-                    if (SwingUtilities.isRightMouseButton(e)) {
-                        showPopupMenu(e);
-                    }
-                }
-            });
-        }
-
-        @Override
-        protected void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            if (showGrid) {
-                drawGrid(g);
-            }
-        }
-
-        private void drawGrid(Graphics g) {
-            int width = getWidth();
-            int height = getHeight();
-
-            g.setColor(Color.LIGHT_GRAY);
-
-            for (int x = 0; x < width; x += gridSize) {
-                g.drawLine(x, 0, x, height);
-            }
-
-            for (int y = 0; y < height; y += gridSize) {
-                g.drawLine(0, y, width, y);
-            }
-        }
-
-        public void setShowGrid(boolean showGrid) {
-            this.showGrid = showGrid;
-            repaint();
-        }
-
-        private void showPopupMenu(MouseEvent e) {
-            JPopupMenu popupMenu = new JPopupMenu();
-
-            JMenuItem smallGridItem = new JMenuItem("Small Grid (20 px)");
-            smallGridItem.addActionListener(event -> setGridSize(20));
-            popupMenu.add(smallGridItem);
-
-            JMenuItem mediumGridItem = new JMenuItem("Medium Grid (30 px)");
-            mediumGridItem.addActionListener(event -> setGridSize(30));
-            popupMenu.add(mediumGridItem);
-
-            JMenuItem largeGridItem = new JMenuItem("Large Grid (40 px)");
-            largeGridItem.addActionListener(event -> setGridSize(40));
-            popupMenu.add(largeGridItem);
-
-            popupMenu.show(this, e.getX(), e.getY());
-        }
-
-        private void setGridSize(int newSize) {
-            this.gridSize = newSize;
-            repaint();
-        }
-    }
-
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("GUI");
@@ -199,4 +129,75 @@ public class Main {
             frame.setVisible(true);
         });
     }
+
+    public static class GridPanel extends JPanel {
+        private int gridSize;
+        private boolean showGrid;
+
+        public GridPanel(int gridSize) {
+            this.gridSize = gridSize;
+            this.showGrid = true;
+
+            addMouseListener(new MouseAdapter() {
+                @Override
+                public void mousePressed(MouseEvent e) {
+                    if (SwingUtilities.isRightMouseButton(e)) {
+                        showPopupMenu(e);
+                    }
+                }
+            });
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            if (showGrid) {
+                drawGrid(g);
+            }
+        }
+
+        private void drawGrid(Graphics g) {
+            int width = getWidth();
+            int height = getHeight();
+
+            g.setColor(Color.LIGHT_GRAY);
+
+            for (int x = 0; x < width; x += gridSize) {
+                g.drawLine(x, 0, x, height);
+            }
+
+            for (int y = 0; y < height; y += gridSize) {
+                g.drawLine(0, y, width, y);
+            }
+        }
+
+        public void setShowGrid(boolean showGrid) {
+            this.showGrid = showGrid;
+            repaint();
+        }
+
+        private void showPopupMenu(MouseEvent e) {
+            JPopupMenu popupMenu = new JPopupMenu();
+
+            JMenuItem smallGridItem = new JMenuItem("Small Grid (20 px)");
+            smallGridItem.addActionListener(event -> setGridSize(20));
+            popupMenu.add(smallGridItem);
+
+            JMenuItem mediumGridItem = new JMenuItem("Medium Grid (30 px)");
+            mediumGridItem.addActionListener(event -> setGridSize(30));
+            popupMenu.add(mediumGridItem);
+
+            JMenuItem largeGridItem = new JMenuItem("Large Grid (40 px)");
+            largeGridItem.addActionListener(event -> setGridSize(40));
+            popupMenu.add(largeGridItem);
+
+            popupMenu.show(this, e.getX(), e.getY());
+        }
+
+        private void setGridSize(int newSize) {
+            this.gridSize = newSize;
+            repaint();
+        }
+    }
+
 }
